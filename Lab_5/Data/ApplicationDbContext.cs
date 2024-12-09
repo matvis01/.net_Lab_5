53583745315b0de4cd7eaac641729c95cf1220b2
matvis01 <mati.siwy@op.pl>
2024-12-09 01:30:01 +0100

    Add project files.

---
+++ Lab_5/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lab_5.Models;
+using System.Collections.Generic;
+
+namespace Lab_5.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
